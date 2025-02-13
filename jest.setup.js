184d08989ee6ff3d38a76159193cf4f94654e6b6
andresisfx <andresrodriguezwg@gmail.com>
2025-02-13 16:37:47 -0500

    Main appp tested

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,6 @@
 // En caso de necesitar la implementación del FetchAPI
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
